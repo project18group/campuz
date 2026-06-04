389ad81f19e14860e595d03fbb7e3d1c3f43c86e
honuKdestine <honudestine933@gmail.com>
2026-06-04 01:03:28 +0000

    UI setup

---
+++ apps/mobile/lib/core/theme/app_text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static TextStyle heading = GoogleFonts.plusJakartaSans(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle title = GoogleFonts.plusJakartaSans(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle body = GoogleFonts.plusJakartaSans(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle caption = GoogleFonts.plusJakartaSans(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
